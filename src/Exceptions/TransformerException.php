1f210be21a66f5d7d58048f48cf22fe95b1692cb
Dizzy <itsdizzy40@gmail.com>
2016-07-29 19:09:53 +0200

    Init

---
+++ src/Exceptions/TransformerException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Dizzy\Transformer\Exceptions;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+class TransformerException extends \Exception
+{
+}
---
+++ src/TransformerServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Dizzy\Transformer;
+
+use Illuminate\Support\ServiceProvider;
+
+class TransformerServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . "/config/transformer.php" => config_path('transformer.php')
+        ]);
+        $this->mergeConfigFrom(
+            __DIR__ . "/config/transformer.php", 'transformer'
+        );
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() { // Nothing to register sorry :( }
+}
